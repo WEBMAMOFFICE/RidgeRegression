6a6d9bf121788f5aa88a796999825fc056612777
Volodymyr Kirichinets <webmamoffice@gmail.com>
2018-04-28 21:32:17 +0300

    change

--- rr0.py
+++ rr0.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+
+X1 = [1, 2.5, 2, 4, 5]
+X2 = [1.5, 3, 2.5, 4.5, 5]
+X3 = [2, 1, 3.5, 4, 5]
+X4 = [1, 3, 2, 5, 4]
+X5 = [1.5, 2.5, 4, 3, 5.5]
+
 y = [1, 2, 3, 4, 5]
-X = [[1.1, 1.3, 1.7, 1.4, 1.9], [2.1, 2.3, 2.7, 2.4, 2.9], [3.1, 3.3, 3.7, 3.4, 3.9],
-     [4.1, 4.3, 4.7, 4.4, 4.9], [5.1, 5.3, 5.7, 5.4, 5.9]]
+X = [[X1[0], X2[0], X3[0], X4[0], X5[0]], [X1[1], X2[1], X3[1], X4[1], X5[1]],
+     [X1[2], X2[2], X3[2], X4[2], X5[2]], [X1[3], X2[3], X3[3], X4[3], X5[3]],
+     [X1[4], X2[4], X3[4], X4[4], X5[4]]]
 lr = linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=True)
 lr.fit(X, y)
 lrR2 = lr.score(X, y)
@@ -13,7 +21,9 @@ print("Coeficient of Determination R² = ", round(lrR2, 2))
 fig = plt.figure()
 ax = fig.add_subplot(111)
 for n in range(0, len(X)):
-    ax.scatter(y, [X[0][n], X[1][n], X[2][n], X[3][n], X[4][n]],  color='red', linewidth=3)
+    n1 = n % 2
+    color = '#%s%s%s%s%s%s' % (n1+5, 8-n1, n1+2, n1, n1+3, n1+4)
+    ax.scatter(y, [X[0][n], X[1][n], X[2][n], X[3][n], X[4][n]],  color=color, linewidth=3)
     ax.scatter(6, sum(lr.coef_) * 6 + lr.intercept_,  color='#FF00FF', linewidth=3)
 Y0 = [sum(lr.coef_) * 0 + lr.intercept_, 6]
 Y6 = [sum(lr.coef_) * 0 + lr.intercept_, sum(lr.coef_) * 6 + lr.intercept_]
